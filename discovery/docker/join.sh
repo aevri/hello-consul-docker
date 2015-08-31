4452df3721e168ecfea9021c3816d9eb1319429b
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-08-31 14:13:55 +0100

    discovery,events: join cluster at start, not after
    'consul agent' supports a '-join' parameter so it's not necessary for us
    to have a separate 'consul join' invocation.
    
    This also removes the need for the retry loop on 'consul join'.

--- discovery/docker/join.sh
+++ discovery/docker/join.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 JOIN_IP=$(getent hosts consul-server | awk '{print $1}')
 echo Will join ${JOIN_IP}
-consul agent -config-dir /config &
-until consul join ${JOIN_IP}; do sleep 1; done
+consul agent -config-dir /config -join ${JOIN_IP} &
 echo hello | nc -lp 80
 consul leave
--- events/docker/bin/startup.sh
+++ events/docker/bin/startup.sh
@@ -2,6 +2,5 @@
 SERVER_IP=$(getent hosts consul-server | awk '{print $1}')
 echo Will connect to ${SERVER_IP}
 python -m SimpleHTTPServer 80 &
-consul agent -config-dir /config &
-until consul join ${SERVER_IP}; do sleep 1; done
+consul agent -config-dir /config -join ${SERVER_IP} &
 wait
